cf0841dce7cfeef6ceda8af4d1cf3771924deffc
capital-hnineithu <capital.hnineithu@gmail.com>
2021-08-19 16:56:41 +0630

    byeithu

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hr_mobile/screens/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF2661FA),
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      // ignore: prefer_const_constructors
+      home: Login(),
+    );
+  }
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,100 @@
+// ignore_for_file: prefer_const_constructors, unnecessary_new
+
+import 'package:flutter/material.dart';
+import 'package:hr_mobile/components/background.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                "CKM HR Login",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green.shade500,
+                    fontSize: 35),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)),
+                ),
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)),
+                ),
+                obscureText: true,
+              ),
+            ),
+            SizedBox(height: size.height * 0.05),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              // ignore: deprecated_member_use
+              child: RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: new BoxDecoration(
+                      borderRadius: BorderRadius.circular(80.0),
+                      gradient: new LinearGradient(
+                          // ignore: prefer_const_literals_to_create_immutables
+                          colors: [
+                            Color.fromARGB(70, 0, 255, 0),
+                            Color.fromARGB(150, 0, 255, 0)
+                          ])),
+                  padding: const EdgeInsets.all(0),
+                  child: Text(
+                    "LOGIN",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
